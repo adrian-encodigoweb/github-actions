b72a3ecf08140657b21f1d1a4c864b954249958f
Adrián Ortega <adrian.ortega@encodigoweb.com>
2021-12-31 12:42:13 +0100

    adfadfa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                         </g>
                     </svg>
                 </div>
-esto es una prueba
+esto es una pruebaddddd
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
